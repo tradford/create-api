dc0320d1eee9fcb4e59e4d78d30b736430a2bdae
Trenton Radford <Trenton.Radford@gmail.com>
2020-10-04 16:25:31 -0600

    fixing mistake with app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-import os
-import pandas
-import requests
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
-    filename = url.split('/')[-1].replace(" ", "_")
-    file_path = os.path.join(dest_folder, filename)
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-    else:
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
-url_to_titanic_data = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
-download(url_to_titanic_data,'./data')
+import json
+import pandas as pd
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/get_json', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        provided_data = request.files.get('file')
+        if provided_data is None:
+            return 'Please enter valid excel format ', 400
+        data = provided_data
+        df = pd.read_csv(data)
+        transformed = df.to_json()
+        result = {
+            'result': transformed,
+        }
+        json.dumps(result)
+        return result
+if __name__ == '__main__':
+    app.run()
